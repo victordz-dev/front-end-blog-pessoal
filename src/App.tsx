cb17ae585b27a0c2ffe8ff2f0d1d462810e08eef
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2026-01-19 09:48:30 -0300

    feat: adicionando estrutura inicial de menu, home e footer, utilizando tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar/>
+      <Home />
+      <Footer/>
+    </>
   );
 }
 export default App;
