f4269d6472d2a57c036e0e99b6101467f5c0eead
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2026-01-19 12:27:34 -0300

    feat: adicionando context, requisição com axios e implementação das models e services

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,18 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('O Usuário foi desconectado com sucesso!')
+    navigate('/')
+  }
+
   return (
     <>
       <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
@@ -12,7 +24,7 @@ function Navbar() {
             Temas
             Cadastrar tema
             Perfil
-            Sair
+            <Link to='' onClick={logout} className="hover:underline">Sair</Link>
           </div>
         </div>
       </div>
