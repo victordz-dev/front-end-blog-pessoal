5bbef38d542a1ee98933ecf28a913f7a7c89b05d
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2026-01-19 13:15:45 -0300

    feat: adicionando o crud de temas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/tema/listaTema/ListaTema'
+import FormTema from './components/tema/formTema/FormTema'
+import DeletarTema from './components/tema/deletarTema/DeletarTema'
 
 function App() {
   return (
@@ -17,6 +20,10 @@ function App() {
               <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
